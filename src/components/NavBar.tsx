51582b2473437ae3bca904585aee1c9de9c8bef9
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-09-19 14:01:30 +0200

    feat: add hover nav link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,27 @@
 import { Component } from "react";
-import Logo from '../assets/images/logo.png'
 import { MenuData } from "./MenuData";
-import "./css/NavBarStyle.css"
+import Logo from "../assets/images/logo.png";
+import MenuIcon from "./assets/images/menu-icon.png";
+import "./css/NavBarStyle.css";
 
 class NavBar extends Component {
-    render(){
-        return (
-        <nav className="NavbarItems">
-            <img src={Logo} alt="logo" style={{width: 160}}/>
-            <ul className="nav-menu">
-                <li>
-                    {MenuData.map((item, index) => {
-                        return(
-                            <li>
-                                <a href={item.url} className={item.cName}>{item.title}</a>
-                            </li>
-                        )
-                    } )}
-                </li>
-            </ul>
-        </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <img src={Logo} alt="logo" style={{ width: 160 }} />
+        <ul className="nav-menu">
+          {MenuData.map((item, index) => {
+            return (
+              <li>
+                <a href={item.url} className={item.cName}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
 }
 export default NavBar;
