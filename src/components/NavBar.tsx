64f0d96b7b76337b2c0a7afaee56412d72132761
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-09-19 11:54:07 +0200

    feat: create components folder

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import logo from '../src/assets/images/logo.png'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import { useRef } from 'react'
+
+
+const NavBar = () => {
+    const navRef = useRef();
+
+    
+
+  return (
+    <header>
+    </header>
+  )
+}
+
+export default NavBar
