6843360b59ac5666d8e597cba9db5c674023852c
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-09-19 15:28:34 +0200

    feat: add shema conceptuel data

--- src/components/MeriseData.tsx
+++ src/components/MeriseData.tsx
@@ -1,1 +1,23 @@
-export const MeriseData = [{}];
+export const MeriseData = [
+  {
+    title: "MCD",
+    url: "#",
+    categorie: "schema conceptuel",
+    description:
+      "Modèle Conceptuel de Données représente les entités, leurs attributs et leurs associations dans un système d'information. ",
+  },
+  {
+    title: "MCT",
+    url: "#",
+    categorie: "schema conceptuel",
+    description:
+      "Modèle Conceptuel de Traitement modélise les processus métier et les flux d'information entre eux.",
+  },
+  {
+    title: "MCC",
+    url: "#",
+    categorie: "schema conceptuel",
+    description:
+      "Modèle Conceptuel de Communication montre les interactions entre les acteurs (utilisateurs) du système et les processus métier.",
+  },
+];
