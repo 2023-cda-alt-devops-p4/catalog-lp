e304c0991a621c5168ad1e6f6969a892c07d470e
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-09-20 12:06:49 +0200

    feat: add same struct to merise page

--- src/components/data/MeriseData.tsx
+++ src/components/data/MeriseData.tsx
@@ -2,21 +2,21 @@ export const MeriseData = [
   {
     title: "MCD",
     url: "#",
-    categorie: "schemas conceptuel",
+    categorie: "schemas conceptuels",
     description:
       "Modèle Conceptuel de Données représente les entités, leurs attributs et leurs associations dans un système d'information. ",
   },
   {
     title: "MCT",
     url: "#",
-    categorie: "schemas conceptuel",
+    categorie: "schemas conceptuels",
     description:
       "Modèle Conceptuel de Traitement modélise les processus métier et les flux d'information entre eux.",
   },
   {
     title: "MCC",
     url: "#",
-    categorie: "schemas conceptuel",
+    categorie: "schemas conceptuels",
     description:
       "Modèle Conceptuel de Communication montre les interactions entre les acteurs (utilisateurs) du système et les processus métier.",
   },
