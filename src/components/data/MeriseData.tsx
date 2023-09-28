2e15134638048574ef898f9e97d7c560c2999487
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-09-28 15:33:45 +0200

    feat: set all data img to merise data

--- src/components/data/MeriseData.tsx
+++ src/components/data/MeriseData.tsx
@@ -1,8 +1,19 @@
+import merise1 from "../../assets/images/merise1.png";
+import merise2 from "../../assets/images/merise2.png";
+import merise3 from "../../assets/images/merise3.png";
+import merise4 from "../../assets/images/merise4.png";
+import merise5 from "../../assets/images/merise5.png";
+import merise6 from "../../assets/images/merise6.png";
+import merise7 from "../../assets/images/merise7.png";
+import merise8 from "../../assets/images/merise8.png";
+import merise9 from "../../assets/images/merise9.png";
+import merise10 from "../../assets/images/merise10.png";
+
 export const MeriseData = [
   {
     id: 1,
     title: "Modèle Conceptuel de Données (MCD)",
-    url: "#",
+    image: merise1,
     categorie: "schemas conceptuels",
     description:
       "Modèle Conceptuel de Données représente les entités, leurs attributs et leurs associations dans un système d'information. ",
@@ -10,7 +21,7 @@ export const MeriseData = [
   {
     id: 2,
     title: "Modèle Conceptuel de Traitement (MCT)",
-    url: "#",
+    image: merise2,
     categorie: "schemas conceptuels",
     description:
       "Modèle Conceptuel de Traitement modélise les processus métier et les flux d'information entre eux.",
@@ -18,7 +29,7 @@ export const MeriseData = [
   {
     id: 3,
     title: "Modèle Conceptuel de Communication (MCC)",
-    url: "#",
+    image: merise3,
     categorie: "schemas conceptuels",
     description:
       "Modèle Conceptuel de Communication montre les interactions entre les acteurs (utilisateurs) du système et les processus métier.",
@@ -26,7 +37,7 @@ export const MeriseData = [
   {
     id: 4,
     title: "Modèle Organisationnel de Données (MOD)",
-    url: "#",
+    image: merise4,
     categorie: "schemas organisationels",
     description:
       "Modèle Organisationnel de Données spécifie la manière dont les données sont stockées et organisées dans le système, notamment les tables de bases de données et leurs relations.",
@@ -34,7 +45,7 @@ export const MeriseData = [
   {
     id: 5,
     title: "Modèle Organisationnel de Traitement (MOT)",
-    url: "#",
+    image: merise5,
     categorie: "schemas organisationels",
     description:
       "Modèle Organisationnel de Traitement définit la façon dont les tâches et les activités sont organisées au sein de l'entreprise, y compris les acteurs responsables de chaque activité.",
@@ -42,7 +53,7 @@ export const MeriseData = [
   {
     id: 6,
     title: "Diagramme de Flux de Données (DFD)",
-    url: "#",
+    image: merise6,
     categorie: "schemas de flux",
     description:
       "Diagramme de Flux de Données représente les flux de données entre les processus, les entités externes et les entrepôts de données.",
@@ -50,7 +61,7 @@ export const MeriseData = [
   {
     id: 7,
     title: "Diagramme de Flux de Contrôle (CFD)",
-    url: "#",
+    image: merise7,
     categorie: "schemas de flux",
     description:
       "Diagramme de Flux de Contrôle détaille la séquence des étapes dans un processus ou une fonction.",
@@ -58,7 +69,7 @@ export const MeriseData = [
   {
     id: 8,
     title: "Diagramme d'Architecture des Systèmes d'Information (ASI)",
-    url: "#",
+    image: merise8,
     categorie: "schemas d'architecture",
     description:
       "Diagramme d'Architecture des Systèmes d'Information montre la structure générale du système d'information, y compris les composants matériels et logiciels.",
@@ -66,7 +77,7 @@ export const MeriseData = [
   {
     id: 9,
     title: "Diagramme de Flux d'Architecture (AFC)",
-    url: "#",
+    image: merise9,
     categorie: "schemas d'architecture",
     description:
       "Diagramme de Flux d'Architecture identifie les flux d'information à travers le système.",
@@ -74,7 +85,7 @@ export const MeriseData = [
   {
     id: 10,
     title: "Diagramme d'Architecture des Organisations (AO)",
-    url: "#",
+    image: merise10,
     categorie: "schemas d'architecture",
     description:
       "Diagramme d'Architecture des Organisations décrit la structure organisationnelle de l'entreprise et les rôles des différents acteurs.",
